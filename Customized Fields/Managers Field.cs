2fe89b640c5ca7d3d05b84cbc239b83e7cdc13c1
Jony Zhu <zhu.jony@gmail.com>
2012-05-07 17:16:31 +0800

    Customized Fields solution added.

---
+++ Customized Fields/Managers Field.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace Customized_Fields
+{
+	public class Managers_Field:SPFieldUser
+	{
+		public Managers_Field(SPFieldCollection fields, string fName) : base(fields, fName) { this.AllowMultipleValues = true; }
+		public Managers_Field(SPFieldCollection fields, string tName, string dName) : base(fields, tName, dName) {  this.AllowMultipleValues = true; }
+		public override string DefaultValue
+		{
+			get {
+				SPWeb web = SPContext.Current.Web;
+				SPUser user = web.CurrentUser;
+				string defaultValue = string.Format("{0};#{1}", user.ID.ToString(), user.Name);
+
+				if (this.SelectionGroup > 0)
+				{
+					SPGroup group = web.Groups[this.SelectionGroup];
+					if ((group != null) && (group.ContainsCurrentUser))
+						return defaultValue;
+				}
+				else
+				{
+					return defaultValue;
+				}
+				return string.Empty;
+			}
+		}
+	}
+}
--- Site Utility/Features/Feature1/Feature1.feature
+++ Site Utility/Features/Feature1/Feature1.feature
@@ -2,5 +2,6 @@
 <feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="612dd407-a823-4a9a-921b-a70fae1961b9" featureId="612dd407-a823-4a9a-921b-a70fae1961b9" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Site Utility Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
   <projectItems>
     <projectItemReference itemId="17c94fe9-911d-4a3d-86e5-ddadf8415a2d" />
+    <projectItemReference itemId="b5b1c4d9-8015-4293-be18-d27aa90cdb48" />
   </projectItems>
 </feature>
