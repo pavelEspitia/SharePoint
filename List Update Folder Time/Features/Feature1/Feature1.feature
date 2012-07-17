552987282ec25c9a3d3a2e75e7b965c78a7e86fd
Jony Zhu <zhu.jony@gmail.com>
2012-07-17 09:20:48 +0800

    list update folder time events receiver added.

---
+++ List Update Folder Time/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="ca29bf02-8639-45e0-bb4e-2da3d669c5bb" featureId="ca29bf02-8639-45e0-bb4e-2da3d669c5bb" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="List Update Folder Time Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="958a0f19-ca4e-4a70-b180-9afae6fa92a1" />
+  </projectItems>
+</feature>
---
+++ List Update Folder Time/Item Changed Event/Item Changed Event.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace List_Update_Folder_Time.Item_Changed_Event
+{
+	/// <summary>
+	/// 列表项事件
+	/// </summary>
+	public class ItemChangedEvent : SPItemEventReceiver {
+		/// <summary>
+		/// 已添加项.
+		/// </summary>
+		public override void ItemAdded(SPItemEventProperties properties) {
+			base.ItemAdded(properties);
+			process_event(properties);
+		}
+
+		/// <summary>
+		/// 已更新项.
+		/// </summary>
+		public override void ItemUpdated(SPItemEventProperties properties) {
+			base.ItemUpdated(properties);
+			process_event(properties);
+		}
+
+		/*
+		/// <summary>
+		/// 已向该项添加附件.
+		/// </summary>
+		public override void ItemAttachmentAdded(SPItemEventProperties properties)
+		{
+			base.ItemAttachmentAdded(properties);
+			process_event(properties);
+		}
+		 */
+
+		protected void process_event(SPItemEventProperties properties) {
+			if (properties.ListItem.Url.Contains("/")) {
+				string url = properties.ListItem.Url.Substring(0, properties.ListItem.Url.LastIndexOf('/'));
+				foreach (SPListItem item in properties.List.Folders) {
+					if (item.Folder.Url.Equals(url)) {
+						update_folder_time(item.Folder);
+						break;
+					}
+				}
+			}
+		}
+		protected void update_folder_time(SPFolder current_folder) {
+			if (current_folder == null || !current_folder.Url.Contains("/")) return;
+			current_folder.SetProperty("Name", current_folder.Name);
+			current_folder.Update();
+			update_folder_time(current_folder.ParentFolder);
+		}
+	}
+}
