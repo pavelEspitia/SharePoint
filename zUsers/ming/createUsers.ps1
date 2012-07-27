2091f2ea6a83a1d9f135f51aff596720fe8d7c9c
Jony Zhu <zhu.jony@gmail.com>
2012-07-27 16:56:34 +0800

    powershell scripts to import users.

---
+++ zUsers/ming/createUsers.ps1
@@ -0,0 +1,78 @@
+# Set-ExecutionPolicy Unrestricted
+# Set-ExecutionPolicy RemoteSigned
+
+param(
+	[string]$userFilePath = $(throw "请指定用户XML文件。"),
+	[string]$localMachineName,
+	[string]$domainName
+)
+
+
+function createUser($server,$fn,$ln,$an,$am){
+	$user = $server.Create("User",$an)
+	$user.SetPassword("mPec2010")
+	$user.Put("Description",$am)
+	$user.Put("FullName",$ln+$fn)
+	$user.SetInfo()
+	write-host $ln $fn $an "，添加完成。"
+	return $user
+}
+
+function createOU($server, $ou){
+	$wOU = $server.Create("OrganizationalUnit","ou="+$ou)
+	$wOU.SetInfo()
+	return $wOU
+}
+
+function createADUser($server,$fn,$ln,$an,$am){
+	$user = $server.Create("User","cn="+$an)
+	$user.SetPassword("mPec2010")
+	$user.Put("Description",$am)
+	$user.Put("FullName",$ln+$fn)
+	$user.SetInfo()
+	write-host $ln $fn $an "，添加完成。"
+	return $user
+}
+if($localMachineName){
+	[ADSI]$server="WinNT://" + $localMachineName
+	[xml]$userFile=get-content $userFilePath
+	foreach($user in $userFile.Es.E)
+	{
+		$wUser = createUser $server $user.Fn $user.LN $user.AN $user.AM
+	}
+}
+
+if($domainName){
+	[xml]$userFile=get-content $userFilePath
+	foreach($user in $userFile.Es.E)
+	{
+		[ADSI]$server="LDAP://ou="+$user.D+",ou="+$user.R+",ou="+$user.O+",DC="+$domainName+",DC=com"
+		if(!$server.Guid){
+			[ADSI]$server="LDAP://ou="+$user.R+",ou="+$user.O+",DC="+$domainName+",DC=com"
+			if(!$server.Guid){
+				[ADSI]$server="LDAP://ou="+$user.O+",DC="+$domainName+",DC=com"
+				if(!$server.Guid){
+					[ADSI]$server="LDAP://DC="+$domainName+",DC=com"
+					createOU $server $user.O
+					[ADSI]$server="LDAP://ou="+$user.O+",DC="+$domainName+",DC=com"
+					createOU $server $user.R
+					[ADSI]$server="LDAP://ou="+$user.R+",ou="+$user.O+",DC="+$domainName+",DC=com"
+					createOU $server $user.D
+					
+				}else{
+					createOU $server $user.R
+					[ADSI]$server="LDAP://ou="+$user.R+",ou="+$user.O+",DC="+$domainName+",DC=com"
+					createOU $server $user.D
+				}
+			}else{
+				createOU $server $user.D
+			}
+			[ADSI]$server="LDAP://ou="+$user.D+",ou="+$user.R+",ou="+$user.O+",DC="+$domainName+",DC=com"
+		}
+		$wUser = createADUser $server $user.Fn $user.LN $user.AN $user.AM
+		if($user.M){
+			$wUser.Put("Manager",$domainName+"\\"+$user.M)
+			$wUser.SetInfo()
+		}
+	}
+}
